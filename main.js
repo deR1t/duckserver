bfc04fa78af93992b4e667460341da140a0e424b
deR1t <ash.g.sim@gmail.com>
2022-02-09 11:19:52 -0500

    Added title.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class Ducky
 		this.y = y;
 		this.vx = vx;
 		this.vy = vy;
-		this.grav = 0.1;
+		this.grav = 0.08;
 		this.shouldDelete = false;
 
 		this.duckimg = document.createElement("img");
@@ -24,8 +24,8 @@ class Ducky
 		this.x += this.vx;
 		this.y += this.vy;
 
-		this.duckimg.style.width = 8 + "vw"
-		this.duckimg.style.height = 8 + "vw"
+		this.duckimg.style.width = 12 + "vw"
+		this.duckimg.style.height = 12 + "vw"
 		this.duckimg.style.left = this.x + "vw";
 		this.duckimg.style.top = this.y + "vh";
 
@@ -56,16 +56,18 @@ console.log(document.body.style.backgroundImage)
 
 setInterval(() => {
 
-	var x = rnd(-10, 110);
-	var y = -8;
-	var vx = rnd(-1, 1)
-	var vy = 0
-	duckies.push(new Ducky(x, y, vx, vy))
+	for (let i = 0; i < 2; i++) {
+		var x = rnd(-10, 110);
+		var y = -12;
+		var vx = rnd(-1, 1)
+		var vy = 0
+		duckies.push(new Ducky(x, y, vx, vy))
+	}
 
 	for (let di = 0; di < duckies.length; di++) {
 		const duck = duckies[di];
 		duck.update()
-		if(duck.y > 108)
+		if(duck.y > 112)
 		{
 			duckiesToDelete.push(di)
 		}
